8469612db32d493d18e8aa61d353fde79c19a921
MB-teacher <MB-teacher@192.168.1.157>
2021-01-02 16:12:55 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -2,11 +2,13 @@ package com.spring.mvc.psi.controller;
 
 import com.spring.mvc.psi.entities.Product;
 import com.spring.mvc.psi.repository.ProductRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,9 +19,12 @@ public class PSIController {
     @Autowired
     private ProductRepository productRepository;
     
-    @GetMapping(value = {"/product"})
-    public String readProduct(Model model) {
+    @GetMapping(value = {"/product", "/product/{id}"})
+    public String readProduct(Model model, @PathVariable Optional<Long> id) {
         Product product = new Product();
+        if(id.isPresent()) {
+            product = productRepository.findOne(id.get());
+        }
         model.addAttribute("product", product);
         model.addAttribute("products", productRepository.findAll());
         return "product";
