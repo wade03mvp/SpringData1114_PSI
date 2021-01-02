1a69965712d4195838e1da22721a8f7ebe5907c2
MB-teacher <MB-teacher@192.168.1.157>
2021-01-02 11:45:23 +0800

    SpringData

--- src/test/java/springdata/crud/CreateOne.java
+++ src/test/java/springdata/crud/CreateOne.java
@@ -1,30 +1,23 @@
-package springdata;
+package springdata.crud;
 
 import com.github.javafaker.Faker;
 import com.spring.mvc.psi.entities.User;
 import com.spring.mvc.psi.repository.UserRepository;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test2 {
+public class CreateOne {
     @Test
     public void t1() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("springdata-config.xml");
         UserRepository userRepository = ctx.getBean(UserRepository.class);
         Faker faker = new Faker();
-        // 新增多筆
-        List<User> users = new ArrayList<>();
-        IntStream.rangeClosed(1, 142).forEach(i -> {
-            User user = new User();
-            user.setName(faker.name().lastName());
-            user.setEmail(faker.internet().emailAddress());
-            user.setBirth(faker.date().birthday());
-            users.add(user);
-        });
-        userRepository.save(users);
+        // 新增單筆
+        User user = new User();
+        user.setName(faker.name().lastName());
+        user.setEmail(faker.internet().emailAddress());
+        user.setBirth(faker.date().birthday());
+        userRepository.save(user);
         System.out.println("OK");
         ctx.close();
                 
