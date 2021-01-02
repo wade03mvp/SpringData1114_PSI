32ae14b942a510e6963fb186de778695a24982d2
MB-teacher <MB-teacher@192.168.1.157>
2021-01-02 14:41:33 +0800

    SpringData

---
+++ src/main/java/com/spring/mvc/psi/entities/Product.java
@@ -0,0 +1,48 @@
+package com.spring.mvc.psi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column
+    private String name;
+    
+    @Column(name = "image", columnDefinition = "clob") // clob 文字型代的大資料, blob 二進位型代的大資料
+    @Lob
+    private String image; // 將圖像轉 base64 後存入
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+}
