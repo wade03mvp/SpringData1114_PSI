a83747e74b47aea19e2f3cd5b9dbdb3a6b709304
MB-teacher <MB-teacher@192.168.1.157>
2021-01-02 14:51:10 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -1,13 +1,15 @@
 package com.spring.mvc.psi.controller;
 
-import javax.enterprise.inject.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/psi")
 public class PSIController {
     
+    @GetMapping(value = {"/product"})
     public String readProduct(Model model) {
         return "product";
     }
