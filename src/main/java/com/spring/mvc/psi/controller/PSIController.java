cb8339cdede64005a99995d15a90da674a13c2ee
MB-teacher <MB-teacher@192.168.1.157>
2021-01-02 16:24:31 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,10 +36,27 @@ public class PSIController {
     public String createProduct(@ModelAttribute("product") Product product) {
         // 將資料存入
         productRepository.save(product);
-        
         // 重導頁面
         return "redirect: ./product";
     }
+    
+    @PutMapping(value = {"/product"})
+    public String updateProduct(@ModelAttribute("product") Product product) {
+        // 修改資料
+        productRepository.saveAndFlush(product);
+        // 重導頁面
+        return "redirect: ./product";
+    }
+    
+    @DeleteMapping(value = {"/product"})
+    public String deleteProduct(@ModelAttribute("product") Product product) {
+        // 刪除資料
+        productRepository.delete(product.getId());
+        // 重導頁面
+        return "redirect: ./product";
+    }
+    
+    
     
     
 }
