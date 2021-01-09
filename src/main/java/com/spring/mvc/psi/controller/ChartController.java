a5cf75c142da92a285066042cde459bcb3fbb786
MB-teacher <MB-teacher@DESKTOP-KVQTFGR.lan>
2021-01-09 14:55:02 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/ChartController.java
+++ src/main/java/com/spring/mvc/psi/controller/ChartController.java
@@ -1,5 +1,7 @@
 package com.spring.mvc.psi.controller;
 
+import com.spring.mvc.psi.repository.Inventory2Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/chart")
 public class ChartController {
     
+    @Autowired
+    private Inventory2Repository inventory2Repository;
+    
     @RequestMapping("/report")
     public String report(Model model) {
+        model.addAttribute("inventories2", inventory2Repository.findAll());
         return "chart/report";
     }
 }
