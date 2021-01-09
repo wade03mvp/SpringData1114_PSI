90a17201f8ea2313c227454b50853a53af83fca6
MB-teacher <MB-teacher@192.168.1.157>
2021-01-09 11:05:01 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/entities/sql/inventory_view_2.sql
+++ src/main/java/com/spring/mvc/psi/entities/sql/inventory_view_2.sql
@@ -6,7 +6,7 @@ SELECT i.id,
        i.PU_QTY-coalesce(i.SA_QTY, 0) qty, -- coalesce 合併(若 i.SA_QTY = null 則顯示 0)
        i.PU_TOTAL/i.PU_QTY as pu_cost , -- 進貨均價 
        i.SA_TOTAL/i.SA_QTY as sa_cost , -- 銷貨均價
-       CAST(i.PU_TOTAL/i.PU_QTY/0.3 AS INTEGER) as price1 ,
-       CAST(i.PU_TOTAL/i.PU_QTY/0.2 AS INTEGER) as price2 
+       CAST(i.PU_TOTAL/i.PU_QTY/0.3 AS INTEGER) as price1 , -- 建議售價 1 (會員價)
+       CAST(i.PU_TOTAL/i.PU_QTY/0.2 AS INTEGER) as price2   -- 建議售價 2 (非會員價)
 FROM INVENTORY i
 
