41319c515fd0f85a1f0106939353ae5f9789b0e0
MB-teacher <MB-teacher@DESKTOP-KVQTFGR.lan>
2021-01-09 13:57:03 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.psi.controller;
 
 import com.spring.mvc.psi.entities.Product;
 import com.spring.mvc.psi.entities.Purchase;
+import com.spring.mvc.psi.entities.Sales;
 import com.spring.mvc.psi.entities.User;
 import com.spring.mvc.psi.repository.Inventory2Repository;
 import com.spring.mvc.psi.repository.InventoryRepository;
@@ -105,6 +106,14 @@ public class PSIController {
         return "purchase";
     }
     
+    // 讀取銷貨
+    @GetMapping(value = {"/sales"})
+    public String readSales(Model model) {
+        model.addAttribute("sales", salesRepository.findAll());
+        model.addAttribute("inventories2", inventory2Repository.findAll());
+        return "sales";
+    }
+    
     // 進貨
     @PostMapping(value = {"/purchase"},
                  consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -126,5 +135,25 @@ public class PSIController {
         return "redirect: ./purchase";
     }
     
+    // 銷貨
+    @PostMapping(value = {"/sales"},
+                 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String createSales(@RequestBody MultiValueMap<String, String> form, HttpSession session) {
+        // 取得表單資料
+        Integer pid = Integer.parseInt(form.getFirst("pid"));
+        Integer quantity = Integer.parseInt(form.getFirst("quantity"));
+        Integer price = Integer.parseInt(form.getFirst("price"));
+        // 取得操作的使用者
+        User user = userRepository.getByName(session.getAttribute("username")+"");
+        // 建立 Sales 物件
+        Sales sales = new Sales();
+        sales.setProduct(productRepository.findOne(pid));
+        sales.setPrice(price);
+        sales.setQuantity(quantity);
+        sales.setUser(user);
+        // 儲存
+        salesRepository.saveAndFlush(sales);
+        return "redirect: ./sales";
+    }
     
 }
