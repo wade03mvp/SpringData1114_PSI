b79b4551902358f8f945f5b0462c793008b1db4a
MB-teacher <MB-teacher@192.168.1.157>
2021-01-09 10:33:10 +0800

    SpringData

---
+++ src/main/java/com/spring/mvc/psi/entities/Purchase.java
@@ -0,0 +1,87 @@
+package com.spring.mvc.psi.entities;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "PURCHASE")
+public class Purchase {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private Integer price; // 買進價格(成本)
+    
+    @Column
+    private Integer quantity; // 買進數量
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    //@Column
+    private Date date = new Date();
+    
+    @ManyToOne
+    @JoinColumn(name = "pid", referencedColumnName = "id")
+    private Product product;
+    
+    @ManyToOne
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
